5ff1a90313e66bfa20343c0c131b954fe23a5f2a
amansayeed <amansayeed@gmail.com>
2020-11-09 11:11:52 +0530

    add blog app and setting the url on the blog section

--- webapp/blog/urls.py
+++ webapp/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home, name="blog-home")
-    path('', views.home, name="blog-home")
+    path('', views.home, name="blog-home"),
+    path('about/', views.about, name="blog-about")
 
 ]
--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def home(request):
     return  HttpResponse('<h1>this is blog home</h1> ')
+
+def about(request):
+    return  HttpResponse('<h1>this is blog about page </h1> ')
 
