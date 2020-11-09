b37e00feddbf8ae3284943b9ae6cb81d798b0e57
amansayeed <amansayeed@gmail.com>
2020-11-09 12:16:18 +0530

    add data in template to make static data

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -5,9 +5,33 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=\
+    [
+        { 'author': 'aman Sayeed',
+            'title': 'this is the first post ',
+          'content' :' lfdkfnlnbfl slkndlfnlfdnbl lksnvflnfdls',
+
+         'date': '20/10/2020',
+
+         },
+
+{ 'author': 'asad  Sayeed',
+            'title': 'this is the secound post ',
+          'content' :' lfdkfnlnbfl slkndlfnlfdnbl sdvksjdhsfssllsh lksnvflnfdls',
+
+         'date': '26/10/2020',
+
+         }
+    ]
+
+context= {
+    'posts': posts
+}
+
+
 def home(request):
     # return  HttpResponse('<h1>this is blog home</h1> ')
-    return  render(request, 'blog/home.html')
+    return  render(request, 'blog/home.html', context)
 
 def about(request):
     # return  HttpResponse('<h1>this is blog about page </h1> ')
